20e16355d66381b035cc4445f90501a6ff596b24
enzo <161404870+Enzocmariano@users.noreply.github.com>
2026-08-17 20:00:04 -0300

    Atividade de equação
    Atividade de equação feita no dia 17/08/2026, primeira equação.

---
+++ equação um.py
@@ -0,0 +1,16 @@
+# Um número somado ao seu dobro resulta em 60
+# x + 2x = 36
+# 3x = 36
+# x = 36/3
+# x = 12
+
+x = 20
+soma = 2*x
+Resultado = x + soma
+
+x + (2*x) 
+3*x 
+x 
+x
+
+print(f"{Resultado}")
