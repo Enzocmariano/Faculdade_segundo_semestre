1c12d3021d8ead625eb2ee29b4e09b2ac7d40c97
enzo <161404870+Enzocmariano@users.noreply.github.com>
2026-08-17 20:01:34 -0300

    Atividade de equação
    Segunda equação das atividades propostas no dia 17/08/2026

---
+++ equação dois.py
@@ -0,0 +1,14 @@
+# A diferença entre um número e sua terça parte é 45.
+
+x = 45
+terca_parte = x / 3
+resultado = x - terca_parte
+
+x - (x / 3)
+(3*x - x) / 3 
+2*x / 3 
+2*x 
+
+print(f"O número é {x}.")
+print(f"Sua terça parte é {terca_parte}.")
+print(f"seu resultado é {resultado}.")
