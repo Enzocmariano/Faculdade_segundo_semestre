359ea148aadf05fef14acf75873a56e1c62af802
enzo <161404870+Enzocmariano@users.noreply.github.com>
2026-09-18 20:35:37 -0300

    Equação 3
    Resolvendo equação com python

---
+++ programming-and-algorithms/atividades/equação3.py
@@ -0,0 +1,10 @@
+idade_p = 20
+idade_j = 3 * idade_p
+idade_c = idade_p
+
+soma_idades = idade_j + idade_p + idade_c
+
+print(f"A idade de Pedro é {idade_p} anos.")
+print(f"A idade de João é {idade_j} anos.")
+print(f"A idade de Carlos é {idade_c} anos.")
+print(f"A soma das idades é {soma_idades} anos.")
